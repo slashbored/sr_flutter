fb2f8de9f3a7e4e9b8f7c76f8956198320fc0c55
slashbored <l.rybka@web.de>
2019-04-19 14:00:29 +0200

    Commit!

--- lib/overView.dart
+++ lib/overView.dart
@@ -20,15 +20,18 @@ class viewOverviewState extends State<viewOverview>{
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('Übersicht'),
-        centerTitle: true,
-      ),
-      drawer: menuDrawer(context),
-      body: new Text('Hier steht die Übersicht.',
-        textAlign: TextAlign.center,
-      )
+    return new WillPopScope(onWillPop: () async => null,
+    child: new Scaffold(
+        appBar: new AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Übersicht'),
+          centerTitle: true,
+        ),
+        drawer: menuDrawer(context),
+        body: new Text('Hier steht die Übersicht.',
+          textAlign: TextAlign.center,
+        )
+    )
     );
   }
 }
