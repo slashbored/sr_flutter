31faa4a2ba3b77f0a02ce77f4c3425b94aedd9b1
slashbored <l.rybka@web.de>
2019-04-20 13:32:08 +0200

    Commit!

--- lib/dataBase.dart
+++ lib/dataBase.dart
@@ -18,7 +18,7 @@ Future buildDatabase() async{
   var results = await connection.query('select * from questions_german');
 
   for (var row in results)  {
-    question.addQuestion(row[0], row[1].toString(), row[2], row[3].toString(), row[4].toString(), row[5].toString(), row[6].toString(), row[7].toString(), row[8].toString());
+    question.addQuestion(row[0], row[1].toString(), row[2], row[3], row[4], row[5], row[6].toString(), row[7].toString(), row[8].toString(), row[9].toString(), row[10].toString(), row[11].toString());
   }
 
   connection.close();
--- lib/question.dart
+++ lib/question.dart
@@ -1,6 +1,9 @@
 class question  {
   int id;
   String descr;
+  int cat_id;
+  int type_id;
+  int subtype_id;
   int time;
   String taska;
   String taskm;
@@ -11,10 +14,10 @@ class question  {
   static int qic = 0;
   static var questionbase = {};
 
-  question(this.id, this.descr, this.time, this.taska, this.taskm, this.tasky, this.taskt, this.taskr, this.taskq);
+  question(this.id,  this.descr, this.cat_id, this.type_id, this.subtype_id, this.time, this.taska, this.taskm, this.tasky, this.taskt, this.taskr, this.taskq);
 
-  question.addQuestion(int id, String descr, int time, String taska, String taskm, String tasky, String taskt, String taskr, String taskq){
-      question newQuestion = new question(id, descr, time, taska, taskm, tasky, taskt, taskr, taskq);
+  question.addQuestion(int id, String descr, int cat_id, int type_id, int subtype_id, int time, String taska, String taskm, String tasky, String taskt, String taskr, String taskq){
+      question newQuestion = new question(id, descr, cat_id, type_id, subtype_id, time, taska, taskm, tasky, taskt, taskr, taskq);
     questionbase[id] = newQuestion;
     qic++;
   }
