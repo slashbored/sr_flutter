40e7d70a001de9e5b0887b4e946021a577fbc45c
slashbored <l.rybka@web.de>
2019-04-20 15:40:36 +0200

    Get cats n stuff from DB.

--- lib/dataBase.dart
+++ lib/dataBase.dart
@@ -15,10 +15,24 @@ Future buildDatabase() async{
     )
   );
 
-  var results = await connection.query('select * from questions_german');
-
-  for (var row in results)  {
+  var questionsFromDB = await connection.query('select * from questions_german');
+  for (var row in questionsFromDB)  {
     question.addQuestion(row[0], row[1].toString(), row[2], row[3], row[4], row[5], row[6].toString(), row[7].toString(), row[8].toString(), row[9].toString(), row[10].toString(), row[11].toString());
+  }
+
+  var categoriesFromDB = await connection.query('select * from categories');
+  for (var row in categoriesFromDB)  {
+    category.addCategory(row[0], row[1].toString(), row[2].toString());
+  }
+
+  var typesFromDB = await connection.query('select * from type');
+  for (var row in typesFromDB)  {
+    type.addType(row[0], row[1].toString());
+  }
+
+  var subtypesFromDB = await connection.query('select * from subtype');
+  for (var row in subtypesFromDB)  {
+    subtype.addSubtype(row[0], row[1].toString());
   }
 
   connection.close();
