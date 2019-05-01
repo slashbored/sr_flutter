4899bfa827a4a231bc46cad5f0645c0924035880
slashbored <l.rybka@web.de>
2019-05-01 15:31:37 +0200

    Emojified categories

--- lib/category.dart
+++ lib/category.dart
@@ -76,6 +76,7 @@ class editCategories extends StatefulWidget{
 
 class editCategoriesState extends State<editCategories>{
 
+  static String amountIndicator;
 
   @override
   Widget build(BuildContext context) {
@@ -98,57 +99,59 @@ class editCategoriesState extends State<editCategories>{
             alignment: Alignment.center,
             heightFactor: 0.75,
             widthFactor: 0.75,
-              child: new ListView(
-                  children: <Widget>[
-                    ListView.builder(itemCount: category.categoryDatabase.length,
-                      itemBuilder: (BuildContext context, int index){
-                      return new ListTile(
-                        title: new Text(category.getCatergoryTitle_german(index+1)),
-                        trailing: new Row(
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.remove_circle),
-                              color: Colors.red,
-                              splashColor: Colors.transparent,
-                              highlightColor: Colors.transparent,
-                              onPressed: () {
-                                if (index+1!=7) {
-                                  if  (category.getCategoryAllowedAmount(index+1)!=0)    {
-                                    category.setCategoryAllowedAmount(index+1, category.getCategoryAllowedAmount(index+1)-1);
-                                    if  (category.getCategoryAllowedAmount(index+1)==0)  {
-                                      category.cbAllowed[index+1] = false;
-                                    }
-                                  }
-                                  setState(() {
-                                    for (bool selectedCategory in category.cbAllowed.values) {
-                                      (selectedCategory)?category.cic++:null;
-                                    }
-                                  });
-                                }
-                              },
-                            ),
-                            Text(category.getCategoryAllowedAmount(index+1).toString()),
-                            IconButton(
-                                icon: Icon(Icons.add_circle),
-                                color: Colors.green,
-                              splashColor: Colors.transparent,
-                              highlightColor: Colors.transparent,
-                                onPressed: () {
-                                  if (index+1!=7) {
-                                    category.setCategoryAllowedAmount(index+1, category.getCategoryAllowedAmount(index+1)+1);
-                                    category.cbAllowed[index+1] = true;
-                                    setState(() {
-                                      for (bool selectedCategory in category.cbAllowed.values) {
-                                        (selectedCategory)?category.cic++:null;
+              child: new Column(
+                children: <Widget>[
+                  new Expanded(child: new ListView(
+                    children: <Widget>[
+                      ListView.builder(itemCount: category.categoryDatabase.length,
+                        itemBuilder: (BuildContext context, int index){
+                          return new ListTile(
+                              title: new Text(category.getCatergoryTitle_german(index+1)),
+                              trailing: new Row(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.remove_circle),
+                                    color: Colors.red,
+                                    splashColor: Colors.transparent,
+                                    highlightColor: Colors.transparent,
+                                    onPressed: () {
+                                      if (index+1!=7) {
+                                        if  (category.getCategoryAllowedAmount(index+1)!=0)    {
+                                          category.setCategoryAllowedAmount(index+1, category.getCategoryAllowedAmount(index+1)-1);
+                                          if  (category.getCategoryAllowedAmount(index+1)==0)  {
+                                            category.cbAllowed[index+1] = false;
+                                          }
+                                        }
+                                        setState(() {
+                                          for (bool selectedCategory in category.cbAllowed.values) {
+                                            (selectedCategory)?category.cic++:null;
+                                          }
+                                        });
                                       }
-                                    });
-                                  }
-                                }
-                            )],
-                          mainAxisSize: MainAxisSize.min,
-                        )
-                        );
-                        /*return new SwitchListTile(
+                                    },
+                                  ),
+                                  Text(getAmountIndicator(category.getCategoryAllowedAmount(index+1))),
+                                  IconButton(
+                                      icon: Icon(Icons.add_circle),
+                                      color: Colors.green,
+                                      splashColor: Colors.transparent,
+                                      highlightColor: Colors.transparent,
+                                      onPressed: () {
+                                        if (index+1!=7&&category.getCategoryAllowedAmount(index+1)<=2) {
+                                          category.setCategoryAllowedAmount(index+1, category.getCategoryAllowedAmount(index+1)+1);
+                                          category.cbAllowed[index+1] = true;
+                                          setState(() {
+                                            for (bool selectedCategory in category.cbAllowed.values) {
+                                              (selectedCategory)?category.cic++:null;
+                                            }
+                                          });
+                                        }
+                                      }
+                                  )],
+                                mainAxisSize: MainAxisSize.min,
+                              )
+                          );
+                          /*return new SwitchListTile(
                             title: new Text(category.getCatergoryTitle_german(index+1)),
                             value: category.cbAllowed[index+1],
                             onChanged: (bool newCBValue) {
@@ -165,14 +168,37 @@ class editCategoriesState extends State<editCategories>{
                               }
                             },
                         );*/
-                      },
-                      shrinkWrap: true,
-                    )
-                  ],
+                        },
+                        shrinkWrap: true,
+                      )
+                    ],
+                  )
+                  ),
+                  new Container(
+                    child: new Text('Erklärung:\n😑 = ausgeschaltet, 😊 = normale Häufigkeit,\n😳 = doppelte Häufigkeit, 😱 = dreifache Häufigkeit', textAlign: TextAlign.center,)
+                  )
+                ],
               )
       )
-      )
+      ),
       );
+  }
+
+  String getAmountIndicator(int Amount) {
+    switch  (Amount)  {
+      case 0: {
+        return '😑';
+      }
+      case 1: {
+        return '😊';
+      }
+      case 2: {
+        return '😳';
+      }
+      case 3: {
+        return '😱';
+      }
+    }
   }
 }
 
